d4ec0c5cb584ca80155c222a7e86de56a0820602
Annelies Bellon <annelies_bellon@hotmail.com>
2019-02-28 14:01:41 +0100

    09_netflix exercise files added

--- 05_swarm_arrays/header.inc.php
+++ 05_swarm_arrays/header.inc.php
@@ -3,7 +3,7 @@
         
        <div><h2>in <strong>East Harlem</strong></h2></div> 
 
-       <a href="sort_distance.php" class="btnSort">sort</a>
+  
     </header>
 
 
---
+++ 09_netflix/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IMDFlix</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+  <div id="netflix">
+  <?php include_once("nav.inc.php"); ?>
+  
+  <div class="collection">
+    <a href="#" class="collection__item" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaoJ20qUVNCzeZnQ7Aq2aBiBdpcJQMG4_u2JhNXpvL4mLXwO2H)">
+    </a>
+  </div>
+  
+</div>
+
+</body>
+</html>
