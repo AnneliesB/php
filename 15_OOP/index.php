4cebaae7eb54b50cc167e024111db69cbf7cb3ef
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-14 17:08:42 +0100

    getters and setters generated

--- 15_OOP/index.php
+++ 15_OOP/index.php
@@ -1,36 +1,30 @@
 <?php 
     class Student{
         private $firstname;
-
-        public function setFirstname($p_sFirstname){
-            if(empty($p_sFirstname)){
-                throw new Exception("Firstname cannot be empty. :)");
-            }
-            $this->firstname = $p_sFirstname;
-            /* return $this; 
-            geeft heel het object terug mee wat handig is voor chainen
-            */
+/**
+         * Get the value of firstname
+         */ 
+        public function getFirstname()
+        {
+                return $this->firstname;
         }
 
-        public function getFirstname(){
-            return $this->firstname;
+        /**
+         * Set the value of firstname
+         *
+         * @return  self
+         */ 
+        public function setFirstname($firstname)
+        {
+                $this->firstname = $firstname;
+
+                return $this;
         }
         
-        /* public function __construct($p_sFirstname){
-            // $p = parameter
-            // s = string
-            if(empty($p_sFirstname)){
-                echo "new phone who dis";
-            } else {
-            $this->firstname = $p_sFirstname;
-            echo "hello " . $this->firstname . " my old friend";
-            }
-        } 
         
-        constructor kan, maar is niet handig omdat we maar 1 ding per 
-        functie willen doen en een constructor zou verschillende soorten data
-        moeten controleren
-        */
+
+        
+        
     }
 
     try{
@@ -42,10 +36,8 @@
     catch(Throwable $t){
         echo $t->getMessage();
         // hier gaan we dan de link vinden naar de exception error message
+
+        
     }
 
-
-   /*  $student1->firstname = "chareltje";
-    echo $student1->firstname;
-    $student2 = new Student(); */
 ?>
