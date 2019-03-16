e7be5fe95fb20f6dcd449327cf984bc37a12990a
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-16 19:15:29 +0100

    Sign up: basic form html setup

---
+++ 16_spotify_faker/signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spotify Faker</title>
+    
+</head>
+<body>
+
+<div>
+    <a href="#"><img src="logo.png" alt="#" width="100px"></a>
+
+    <a href="#" class="btnFb">SIGN UP WITH FACEBOOK</a>
+
+    <form action="" method="post">
+
+        <div class="form__field">
+            <label for="email"></label>
+            <input type="text" id="email" name="email">
+        </div>
+
+        <div class="form__field">
+            <label for="password"></label>
+            <input type="text" id="password" name="password">
+        </div>
+
+        <div class="form__field">
+            <label for="passwordConfirmation"></label>
+            <input type="text" id="passwordConfirmation" name="passwordConfirmation">
+        </div>
+
+        <div class="form__field">
+            <input type="submit" value="Sign up">
+        </div>
+    
+    </form>
+</div>
+    
+</body>
+</html>
