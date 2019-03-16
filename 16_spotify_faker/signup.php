aa495d1c9ae4ee839d40d3cf48d3c806f0dd81d9
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-16 19:42:08 +0100

    Sign up: css added

--- 16_spotify_faker/signup.php
+++ 16_spotify_faker/signup.php
@@ -5,37 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Spotify Faker</title>
-    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div>
-    <a href="#"><img src="logo.png" alt="#" width="100px"></a>
+    <div class="top">
+        <a href="#" class="logo"><img src="logo.png" alt="#"></a>
 
-    <a href="#" class="btnFb">SIGN UP WITH FACEBOOK</a>
+        <a href="#" class="btnFb">SIGN UP WITH FACEBOOK</a>
+    </div>
+    <div class="container">
+        <form action="" method="post">
 
-    <form action="" method="post">
+            <div class="form__field">
+                <label for="email">EMAIL</label>
+                <input type="text" id="email" name="email">
+            </div>
 
-        <div class="form__field">
-            <label for="email"></label>
-            <input type="text" id="email" name="email">
-        </div>
+            <div class="form__field">
+                <label for="password">PASSWORD</label>
+                <input type="text" id="password" name="password">
+            </div>
 
-        <div class="form__field">
-            <label for="password"></label>
-            <input type="text" id="password" name="password">
-        </div>
+            <div class="form__field">
+                <label for="passwordConfirmation">PASSWORD CONFIRMATION</label>
+                <input type="text" id="passwordConfirmation" name="passwordConfirmation">
+            </div>
 
-        <div class="form__field">
-            <label for="passwordConfirmation"></label>
-            <input type="text" id="passwordConfirmation" name="passwordConfirmation">
-        </div>
-
-        <div class="form__field">
-            <input type="submit" value="Sign up">
-        </div>
-    
-    </form>
+            <div class="form__field">
+                <input type="submit" value="Sign up">
+            </div>
+        
+        </form>
+    </div>
 </div>
     
 </body>
