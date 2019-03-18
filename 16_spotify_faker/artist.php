c503539a5c8b7c9c904a0b12a51e4f30ba5433d4
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-18 17:31:52 +0100

    attempt second forEach loop for tracks || unsuccessful

--- 16_spotify_faker/artist.php
+++ 16_spotify_faker/artist.php
@@ -3,7 +3,13 @@ $conn = new PDO("mysql:host=localhost;dbname=spotify", "root", "root");
 $id = (int) $_GET['id'];
 $statement = $conn -> prepare("SELECT * from albums where artist_id='$id'");
 $statement-> execute();
+$albumId = $statement->fetch()['id'];
 $albums = $statement->fetchAll();
+
+$statement2 = $conn -> prepare("SELECT * from tracks where album_id='$albumId'");
+$statement2 -> execute();
+$tracksId = $statement->fetch()['album_id'];
+$tracks = $statement->fetchAll();
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -23,6 +29,12 @@ $albums = $statement->fetchAll();
           <div>
             <h3> <?php echo $a['title']; ?></h3>
             <img src="<?php echo $a['cover']; ?>" alt="#">  
+            <?php foreach($tracks as $t): ?>
+
+                <?php if($tracksId == $albumId) : ?>
+                    <p><?php echo $t['title']; ?></p>
+                <?php endif; ?>
+            <?php endforeach; ?>
           </div>
         <?php endforeach; ?>
       </div>
