6fea3dbcac27c0bff469b9f4e942180c706c87c1
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-21 14:58:01 +0100

    ticket gets printed with random seat number

--- 17_airlineimd/classes/Ticket.php
+++ 17_airlineimd/classes/Ticket.php
@@ -27,5 +27,19 @@
 
                 return $this;
         }
+
+        public function book(){
+            
+            $this->setSeat(rand(1, 60));
+
+
+            // connectie + query
+        }
+
+        public function getTicket(){
+            $ticket = "<h3>Firstname: " . $this->getFirstname() . "</h3>";
+            $ticket .= "<p>Seat: " . $this->getSeat() . "</p>";
+            return $ticket;
+        }
     }
 
